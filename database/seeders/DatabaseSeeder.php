cc83deaf1ac64f0a48dc29f494e857aacb2c25de
bianc <bianc@host.docker.internal>
2024-10-11 00:15:40 -0300

    criacao do model proposal

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,15 +3,13 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Proposal;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         User::factory()
@@ -19,7 +17,12 @@ class DatabaseSeeder extends Seeder
             ->create();
 
         User::query()->inRandomOrder()->limit(10)->get()
-            ->each(fn(User $u) => Project::factory()->create(['created_by' => $u->id]))
+            ->each(function (User $u) {
+                $project = Project::factory()->create(['created_by' => $u->id]);
+                Proposal::factory()
+                    ->count(random_int(4, 45))
+                    ->create(['project_id' => $project->id]);
+            })
         ;
 
     }
