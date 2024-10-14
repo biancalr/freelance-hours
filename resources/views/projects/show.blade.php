88f240f2b6baa2480938baddf5d7ee710a4f2604
bianc <bianc@host.docker.internal>
2024-10-14 02:47:46 -0300

    salvando proposta

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -2,7 +2,7 @@
 
     <div class="grid grid-cols-3 gap-6">
         <livewire:projects.show :$project />
-
         <livewire:projects.proposals :$project />
+        <livewire:proposals.create />
     </div>
 </x-layouts.app>
